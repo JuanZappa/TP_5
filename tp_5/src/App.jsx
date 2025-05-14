cc45df1705fb9b518844626da6cb4c32fa7e1075
Juan Manuel Zappalorto <juan.zappalorto@grupoceta.com.ar>
2025-05-13 23:17:42 -0300

    push_inicial

---
+++ tp_5/src/App.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register  from "./pages/Register";
+import ProductosLista from "./components/ProductosLista"; // Asegurate de la ruta correcta
+import ProductoDetalle from "./components/ProductoDetalle";
+
+function App() {
+  return (
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/productos" element={<ProductosLista />} />
+        <Route path="/productos/:id" element={<ProductoDetalle />} />
+        <Route path="/detalle/:id" element={<ProductoDetalle />} />
+      </Routes>
+  );
+}
+
+export default App;
